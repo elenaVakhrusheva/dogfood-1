21193811601b0d1997a8b41d3192e0ad552cd239
«Ваше <elena.vakhrusheva92@yandex.ru>
2022-12-08 15:12:37 +0400

    1

--- src/index.js
+++ src/index.js
@@ -1,40 +1,25 @@
-import React from "react";
+/* import React from "react";
 import reactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
  import App from "./App";
-// import 'antd/dist/antd.css'; 
-// import AppAnt from "./components/AppAnt/app-ant";
 import "./style.css";
-//import AppMUI from './components/AppMUI/app-mui';
-/*  import { ProductPage } from "./pages/ProductPage" */
 import 'regenerator-runtime/runtime'
 
-/*  reactDom.render(
-	<ProductPage/>,
-	document.querySelector("#root")
-	
-);  */
 reactDom.render((
   <BrowserRouter>
     <App />
   </BrowserRouter>
 ), document.getElementById('root'))
 
-/*  reactDom.render(
-	<ProductPage/>,
-	document.querySelector("#root")
-	
-);  */
+ */
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import "./style.css";
 
-
- /* reactDom.render(
-	<AppMUI/>,
-	document.querySelector("#root")
-	
-);  */
-
-/* reactDom.render(
-	<AppAnt/>,
-	document.querySelector("#root")
-	
-); */
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+   document.getElementById('root'));
