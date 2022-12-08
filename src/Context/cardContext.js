1c08a18676638e06411cc7400b7365f5351b2bd8
«Ваше <elena.vakhrusheva92@yandex.ru>
2022-12-08 14:09:28 +0400

    Updates

--- src/Context/cardContext.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-export const CardContext = createContext(null);
-CardContext.displayName = 'CardContext';
--- src/utils/product.js
+++
@@ -1,9 +0,0 @@
-export const isLiked = (likes, userId) => likes.some(id => id === userId);
-
-export const calcDiscountPrice= (price, discount) =>{
-  return Math.round(price - price*discount / 100);
-}
-
-export const createMarkup = (textToHtml) => {
-  return {__text:textToHtml}
-}
